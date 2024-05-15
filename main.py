0192f9cc117352c9975ecee9f8dc8f9ae1bdc049
VenyeZhao <154851454+VenyeZhao@users.noreply.github.com>
2024-05-15 18:14:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import pandas as pd
 st.text_input("y = ", key = "eqn")
 st.text_input("x = ", key = "x")
 
-def to_float(x):
-  if not (isinstance(x, str)):
-    x = str(x)
-  for i in range(len(x)):
-    if (x[i] != "0" or x[i] != "1" or x[i] != "2" or x[i] != "3" or x[i] != "4" or x[i] != "5" or x[i] != "6" or x[i] != "7" or x[i] != "8" or x[i] != "9" or x[i] != "."):
-      temp = x.split(x[i])
-      x = temp[0]
-      for j in range(1, len(temp)):
-        x += temp[j]
-      i -= 1
-  return float(x)
+#def to_float(x):
+  #if not (isinstance(x, str)):
+    #x = str(x)
+  #for i in range(len(x)):
+    #if (x[i] != "0" or x[i] != "1" or x[i] != "2" or x[i] != "3" or x[i] != "4" or x[i] != "5" or x[i] != "6" or x[i] != "7" or x[i] != "8" or x[i] != "9" or x[i] != "."):
+      #temp = x.split(x[i])
+      #x = temp[0]
+      #for j in range(1, len(temp)):
+        #x += temp[j]
+      #i -= 1
+  #return float(x)
 
 # Convert the eqn to a list of digits and operations.
 def str_to_eqn(eqn_str):
