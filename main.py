dab8fc075d7547aae10278300546b262f11dd5e4
VenyeZhao <154851454+VenyeZhao@users.noreply.github.com>
2024-05-15 18:06:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -120,9 +120,9 @@ def evaluate(eqn, x):
       eqn.pop(i)
       eqn.pop(i)
       i -= 2
-    elif (isinstance(eqn[i], float) or isinstance(eqn[i], int)) and (isinstance(eqn[i + 1], float) or isinstance(eqn[i + 1], int)):
-      eqn[i] = eqn[i] * eqn[i + 1]
-      eqn.pop(i + 1)
+    #elif (isinstance(eqn[i], float) or isinstance(eqn[i], int)) and (isinstance(eqn[i + 1], float) or isinstance(eqn[i + 1], int)):
+      #eqn[i] = eqn[i] * eqn[i + 1]
+      #eqn.pop(i + 1)
     i += 1
   # Addition and Subtraction
   i = 0
