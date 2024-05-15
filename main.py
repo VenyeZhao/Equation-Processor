ea2d3e1b9e1d439f1e3c8be9ad3c066ce6d69beb
VenyeZhao <154851454+VenyeZhao@users.noreply.github.com>
2024-05-15 18:13:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def to_float(x):
     x = str(x)
   for i in range(len(x)):
     if (x[i] != "0" or x[i] != "1" or x[i] != "2" or x[i] != "3" or x[i] != "4" or x[i] != "5" or x[i] != "6" or x[i] != "7" or x[i] != "8" or x[i] != "9" or x[i] != "."):
-      x.pop(i)
+      temp = x.split(x[i])
+      x = temp[0]
+      for j in range(1, len(temp)):
+        x += temp[j]
       i -= 1
   return float(x)
 
