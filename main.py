03c52a6465c284c071c757723a7cb188c4b5bb03
VenyeZhao <154851454+VenyeZhao@users.noreply.github.com>
2024-05-17 20:02:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def remove_non_num(x):
   i = 0
   while i < len(x):
     if (x[i] != "0" or x[i] != "1" or x[i] != "2" or x[i] != "3" or x[i] != "4" or x[i] != "5" or x[i] != "6" or x[i] != "7" or x[i] != "8" or x[i] != "9" or x[i] != "."):
-      temp = x.split(x[i])
+      temp = x.split(x[i], 1)
       x = temp[0]
-      for j in range(1, len(temp)):
+      for j in range(2, len(temp)):
         st.write(x)
         x += temp[j]
     i += 1
