5e1f212322071b5c2e8a3fe50a3a44dee7a3642f
VenyeZhao <154851454+VenyeZhao@users.noreply.github.com>
2024-05-17 19:43:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def str_to_eqn(eqn_str):
     elif (eqn_str[i] == "x"):
       expression.append("x")
     else:
-      res_str = str[:i] +  str[i + 1:]
+      eqn_str = str[:i] +  str[i + 1:]
     i += 1
   return expression
 
